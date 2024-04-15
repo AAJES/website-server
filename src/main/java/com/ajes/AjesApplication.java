3df148ca020f190de30c5d0748ad5ef1f3d57795
Saikumar guduru <87139133+skg1312@users.noreply.github.com>
2024-04-15 22:58:41 +0530

    Update AjesApplication.java

--- src/main/java/com/ajes/AjesApplication.java
+++ src/main/java/com/ajes/AjesApplication.java
@@ -9,11 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class AjesApplication {
 
-	@Bean
-	public JwtFilter jwtFilter() {
-		return new JwtFilter();
-	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AjesApplication.class, args);
 	}
