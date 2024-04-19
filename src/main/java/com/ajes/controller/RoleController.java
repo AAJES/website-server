299ff419c1588c9886b3216b7f50108b5fb8ef2e
Saikumar guduru <87139133+skg1312@users.noreply.github.com>
2024-04-19 11:56:19 +0530

    Update RoleController.java

--- src/main/java/com/ajes/controller/RoleController.java
+++ src/main/java/com/ajes/controller/RoleController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/ajes")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://project-client-kappa.vercel.app/")
 public class RoleController {
 
     @Autowired
